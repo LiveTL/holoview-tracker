90149d0eed4a55c3d1d3d30b88702292aebde3f2
Aaron Thapa <notsmart98@gmail.com>
2021-02-27 21:33:37 -0500

    it works~!

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ from pprint import pprint, pformat
 from requests.sessions import Session
 
 import os
+from time import sleep
+from datetime import datetime
 
 s = Session()
-f = open("thing.txt", "w", encoding="utf-8")
+f = open("out.txt", "a", encoding="utf-8")
 
 def main():
-    getLiveResponse = getLive()
-    f.write(str(getLiveResponse))
-    idviews = [(x['channel']['yt_channel_id'], x['live_viewers']) for x in getLiveResponse['live']]
-    pprint(idviews)
+    while True:
+        line = str((datetime.now().strftime('%Y-%m-%d %H:%M:%S'), [(x['channel']['name'],x['channel']['yt_channel_id'], x['live_viewers']) for x in getLive()['live']]))
+        print(line)
+        f.write('\n' + line)
+        f.flush()
+        sleep(5)
 
 def getLive():
     return s.get('https://api.holotools.app/v1/live').json()
